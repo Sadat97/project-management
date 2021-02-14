4f87c957b7c97dbc0e0f24bcd9ea7c36381ec814
anwar <mac.mem65@gmail.com>
2021-02-14 04:24:43 +0200

    added mail template and letter-opener gem

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class TicketsMailer < ApplicationMailer
+  def due_date_reminder(ticket)
+    @ticket = ticket
+    mail(to: @ticket.assigned_user, subject: "Ticket #{@ticket.id} : #{@ticket.title} due date reminder!")
+  end
 end
